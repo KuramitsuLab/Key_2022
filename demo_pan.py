1f1c9aa726fb75d68cde3a93e1932dd43b7efff6
Nanako Takizawa <nanako.takizawa11@gmail.com>
2022-11-30 17:10:47 +0900

    commit

--- demo_pan.py
+++ demo_pan.py
@@ -2,8 +2,10 @@ import streamlit as st
 from streamlit_key2 import my_component
 import json
 
+
 #アンケート
-st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
+year = st.radio(label = "あなたのプログラミング経験について当てはまるものをお選びください。", 
+    options = ('経験なし', '歴1年ほど', '歴2~3年ほど','歴3年以上or業務で扱っている'))
 
 
 #キーデータ収集
@@ -13,16 +15,30 @@ st.markdown(f"You've typed {value}")
 
 #ログをjsnol形式へ
 with open('keylog.jsonl', 'a') as f:
-    f.write(value)
-    f.write('\n')
+    if value == "":
+        pass
+    else:
+        dict = {'year':year, 'value':value}#{"year":"経験なし","value":"15114 p 160 r 71 i 136 n 154 t"}
+        json.dump(dict, f, separators=(',',':'),ensure_ascii=False)
+        f.write('\n')
 
 
-#モデルの入力作成
-
+#入力データ作成
+idx1 = value.find(" ")
+data = value[idx1+1:]#Eisu 414 p 299 r 115 i 169 n 126 t
+print(data)
 
 #予測
 
 
+pred = 1 #結果
+
 #結果を表示
-
-
+if pred == 0:
+    st.write("頑張りましょう！")
+elif pred == 1:
+    st.write("よくできました")
+elif pred == 2:
+    st.write("大変よくできました")
+elif pred == 3:
+    st.write("素晴らしい")
