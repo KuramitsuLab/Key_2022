975119f7b5248125d691ad6ed7ffc13498c9afc1
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-11-30 14:43:04 +0900

    fix

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
 from streamlit_key import my_component
 
-value = my_component('Hi')
+value = my_component()
+
 st.markdown(f"You've typed {value}")
 
